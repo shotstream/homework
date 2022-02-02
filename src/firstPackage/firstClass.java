e9b8293a8f2f19ff93659dda1cccbff6f52eb95f
shotstream <1372580@mail.ru>
2022-02-02 21:31:52 +0300

    first commit

---
+++ src/firstPackage/firstClass.java
@@ -0,0 +1,7 @@
+package firstPackage;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, Denis!!!");
+    }
+}
---
+++ src/firstPackage/secondClass.java
@@ -0,0 +1,7 @@
+package firstPackage;
+
+public class secondClass {
+    public static void main(String[] args) {
+        System.out.println("This is easy!!!");
+    }
+}
